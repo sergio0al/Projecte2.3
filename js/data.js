9b9c58ec6d307c21d471a51f2a356535dee01c17
Sergio Alonso <alonsorojalssergio@fpllefia.com>
2026-03-20 15:13:01 +0100

    data index

---
+++ js/data.js
@@ -0,0 +1,46 @@
+export const destinacionsDestacades = [
+  {
+    id: "australia",
+    nom: "Australia",
+    imatgeUrl: "/images/locations/australia.png",
+    propietatsCount: 2246,
+  },
+  {
+    id: "japan",
+    nom: "Japan",
+    imatgeUrl: "/images/locations/japan.png",
+    propietatsCount: 1278,
+  },
+  {
+    id: "new-zealand",
+    nom: "New Zealand",
+    imatgeUrl: "/images/locations/newzealand.png",
+    propietatsCount: 480,
+  },
+  {
+    id: "greece",
+    nom: "Greece",
+    imatgeUrl: "/images/locations/greece.png",
+    propietatsCount: 320,
+  },
+];
+
+
+
+export const ofertes = [
+  {
+    title: "Sydeny’s 10 most fashionable 5 star hotels",
+    description: "Browse the fastest growing tourism sector in the heart of Australia tourism capital ....",
+    imatgeUrl: "/images/travel-inspirations/starts-hotels.png"
+  },
+  {
+    title: "Top cities for Vegan Travellers",
+    description: "Top sites where you do not have to worry about being a vegan. Our tourist guide is here...",
+    imatgeUrl: "/images/travel-inspirations/vegan-travellers.png"
+  },
+  {
+    title: "World’s top destinations during and post covid timeline",
+    description: "Pandemic is still intact and will be here for a longer time. Here’s where your next destination...",
+    imatgeUrl: "/images/travel-inspirations/destinations.png"
+  },
+]
---
+++ js/main.js
@@ -0,0 +1,25 @@
+import { destinacionsDestacades } from "./data.js";
+import { ofertes } from "./data.js";
+
+
+const destinacionsDestacadesDiv = document.querySelector('#destinacionsDestacadesDiv');
+for(let i = 0; i < destinacionsDestacades.length; i++){
+   destinacionsDestacadesDiv.innerHTML += `
+    <article>
+        <img src="${destinacionsDestacades[i].imatgeUrl}" alt="${destinacionsDestacades[i].nom}">
+        <h4 class="name">${destinacionsDestacades[i].nom}</h4>
+        <p class="properties">${destinacionsDestacades[i].propietatsCount} properties</p>
+    </article>`; 
+}
+
+
+const ofertesDiv = document.querySelector('#ofertesDiv')
+for(let i = 0; i < ofertes.length; i++){
+    ofertesDiv.innerHTML += `<article>
+    <img src="${ofertes[i].imatgeUrl}">
+    <div>
+    <h4>Sydeny’s 10 most fashionable 5 star hotels</h4>
+    <p>Browse the fastest growing tourism sector in the heart of Australia tourism capital ....</p>
+    </div>
+    </article>`
+}
