ffb03b05efd834a547a4e1e05224742d4c6d1883
Sergio Alonso <alonsorojalssergio@fpllefia.com>
2026-03-29 16:04:32 +0200

    result hecho

--- js/result.js
+++ js/result.js
@@ -7,29 +7,30 @@ for(let i = 0; i < searchResultsData.length; i++){
     <article class="card">
         <div class="margin">
             <div>
-                <img src="images/resultados/LakesideMotelWarefront.png" alt="Lakeside Motel Warefront">
+                <img src="images/resultados/${searchResultsData[i].photo}.png" alt="Lakeside Motel Warefront">
             </div>
             <div class="card-content">
                 <div class="left">
-                    <h3>Lakeside Motel Warefront</h3>
+                    <h3>${searchResultsData[i].name}</h3>
                     <div class="ratings">
                         <img src="images/icons/rating.png" alt="stars">
-                        <p>4.5 (1200 Reviews)</p>
+                        <p>${searchResultsData[i].rating} (${searchResultsData[i].reviewsCount} Reviews)</p>
                     </div>
-                    <p class="texto1">Live a little and celbrate with champagne</p>
-                    <p class="texto2">Reats include a glass of French champagne, parking and a late checkout. Gym included. Flexible cancellation applies</p>
+                    <p class="texto1">${searchResultsData[i].text}</p>
+                    <p class="texto2">${searchResultsData[i].description}</p>
                     <a class="button" href="detail.html">See availability</a>
                 </div>
                 <div class="right">
                     <div class="offerd">
-                        <p class="discount-15">Book now and receive 15% off</p>
+                        ${(searchResultsData[i].promo == 15) ? `<p class="discount-15">Book now and receive 15% off</p>` : ""}
+                        ${(searchResultsData[i].promo == 30) ? `<p class="discount-30">Receive 30% discount on a restaurant</p>` : ""}
                     </div>
                     <div class="price-section">
-                        <p class="discount">5% off</p>
+                        ${(searchResultsData[i].discount) ? `<p class="discount">${searchResultsData[i].discount}% off</p>` : ""}
                         <p class="rooms">1 room 2 days</p>
                         <div class="div-price">
-                            ${(searchResultsData[i].descuento) ? `<p class="price-discount">$150</p>` : ""}
-                            <p class="price">$130</p>
+                            ${(searchResultsData[i].oldPrice) ? `<p class="price-discount">$${searchResultsData[i].oldPrice}</p>` : ""}
+                            <p class="price">$${searchResultsData[i].price}</p>
                         </div>
                         <p class="taxes">Includes taxes and fees</p>
                     </div>
