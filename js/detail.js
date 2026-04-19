7971adf6fe1e09fd25e3a9fed754e40bd80d2012
Sergio Alonso <alonsorojalssergio@fpllefia.com>
2026-04-19 23:01:44 +0200

    fin DetailSupaBase

--- js/detail.js
+++ js/detail.js
@@ -1,47 +1,71 @@
-import { detailData } from "./data.js";
+let supabaseUrl = "https://ndxpcylicovkaaoxydhk.supabase.co";  
+let supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5keHBjeWxpY292a2Fhb3h5ZGhrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzYzNDMwMTAsImV4cCI6MjA5MTkxOTAxMH0.eCodC6u0WsPGsJk-CxBTRdyXA9E0xgwxLrWiNgjYWpE";
+let client = supabase.createClient(supabaseUrl, supabaseAnonKey);
 
-const facilitiesDiv = document.querySelector(".display-flex");
-facilitiesDiv.innerHTML = `<div class="columna1"></div><div class="columna2"></div>`;
+async function cargarSupaBase(){
+    let facilitiesRes = await client.from("facilities").select("*")
+    let facilities = facilitiesRes.data || []
+    generarFacilities(facilities)
+
+    let areaRes = await client.from("area").select("*")
+    let area = areaRes.data || []
+    generarArea(area)
+
+    let hotelDetailRes = await client.from("hotelDetail").select("*")
+    let hotelDetail = hotelDetailRes.data || []
+    generarHotelDetail(hotelDetail)
+}
 
 
-const columna1 = document.querySelector(".columna1");
-const columna2 = document.querySelector(".columna2");
-let select = columna1;
+function generarFacilities(facilities){
+    const columna1 = document.querySelector(".columna1");
+    const columna2 = document.querySelector(".columna2");
+    let select = columna1;
 
-for (let i = 0; i < detailData.facilities.length; i++) {
-    
-    if (i < 3) {
-        select = columna1
-    } else {
-        select = columna2
+    const facilitiesContainer = document.querySelector(".display-flex");
+    for(let i = 0; i < facilities.length; i++){
+
+        if (i < 3) {
+            select = columna1
+        } else {
+            select = columna2
+        }
+
+        select.innerHTML += 
+        `<div class="flex">
+            <img src="images/icons/${facilities[i].id}.svg" alt="${facilities[i].text}" width="22" height="22">
+            <p>${facilities[i].text}</p>
+        </div>`
+
     }
-
-    select.innerHTML += `
-        <div class="flex">
-            <img src="images/icons/${detailData.facilities[i].id}.svg" alt="${detailData.facilities[i].text}" width="22" height="22">
-            <p>${detailData.facilities[i].text}</p>
-        </div>
-    `
 }
 
-
-const exploreDiv = document.querySelector(".right");
-for (let i = 0; i < detailData.area.length; i++) {
-    exploreDiv.innerHTML += ` 
-    <div class="flex1">
-        <div class="flex2">
-            <img src="images/icons/${detailData.area[i].icon}.svg" alt="${detailData.area[i].icon}" width="16" height="16">
-            <p>${detailData.area[i].text}</p>
-        </div>
-        <p>${detailData.area[i].distance} min drive</p>
-    </div>`
+function generarArea(area){
+    const areaContainer= document.querySelector(".right");
+    for (let i = 0; i < area.length; i++) {
+        areaContainer.innerHTML += ` 
+        <div class="flex1">
+            <div class="flex2">
+                <img src="images/icons/${area[i].icon}.svg" alt="${area[i].icon}" width="16" height="16">
+                <p>${area[i].text}</p>
+            </div>
+            <p>${area[i].distance} min drive</p>
+        </div>`
+    }
 }
 
-document.querySelector("#divPhoto1").innerHTML = `<img src="${detailData.hotel.photo1}" width="820" height="452" alt="photo1">`
-document.querySelector("#divPhoto2").innerHTML = `<img src="${detailData.hotel.photo2}" width="820" height="452" alt="photo1">`
-document.querySelector("#divPhoto3").innerHTML = `<img src="${detailData.hotel.photo3}" width="820" height="452" alt="photo1">`
+function generarHotelDetail(hotelDetail){
+    document.querySelector("#divPhoto1").innerHTML = `<img src="${hotelDetail[0].photo1}" width="820" height="452" alt="photo1">`
+    document.querySelector("#divPhoto2").innerHTML = `<img src="${hotelDetail[0].photo2}" width="820" height="452" alt="photo1">`
+    document.querySelector("#divPhoto3").innerHTML = `<img src="${hotelDetail[0].photo3}" width="820" height="452" alt="photo1">`
 
-document.querySelector(".name-hotel").innerHTML = detailData.hotel.name
-document.querySelector(".ratings").innerHTML = `<img src="images/icons/rating.png" alt="stars" height="20"><p>${detailData.hotel.rating} (${detailData.hotel.reviewsCount} Reviews)</p>`
-document.querySelector(".location").innerHTML = `<img src="images/icons/location.svg" alt="location" height="20"><p>${detailData.hotel.direction}</p>`
-document.querySelector("#description").innerHTML += `<p style="white-space: pre-line;">${detailData.hotel.description}</p>`
+    document.querySelector(".name-hotel").innerHTML = hotelDetail[0].name
+    document.querySelector(".ratings").innerHTML = `<img src="images/icons/rating.png" alt="stars" height="20"><p>${hotelDetail[0].rating} (${hotelDetail[0].reviewsCount} Reviews)</p>`
+    document.querySelector(".location").innerHTML = `<img src="images/icons/location.svg" alt="location" height="20"><p>${hotelDetail[0].direction}</p>`
+    document.querySelector("#description").innerHTML += `<p style="white-space: pre-line;">${hotelDetail[0].description}</p>`
+}
+
+cargarSupaBase()
+
+
+
--- js/myTrips.js
+++ js/myTrips.js
@@ -1,46 +1,66 @@
-import { myTripsData } from "./data.js"
-import { destinacionsDestacades } from "./data.js"
+let supabaseUrl = "https://ndxpcylicovkaaoxydhk.supabase.co";  
+let supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5keHBjeWxpY292a2Fhb3h5ZGhrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzYzNDMwMTAsImV4cCI6MjA5MTkxOTAxMH0.eCodC6u0WsPGsJk-CxBTRdyXA9E0xgwxLrWiNgjYWpE";
+let client = supabase.createClient(supabaseUrl, supabaseAnonKey)
 
 
-const tripsDiv = document.querySelector("#tripsDiv")
-for(let i = 0; i < myTripsData.length; i++){
-    tripsDiv.innerHTML += `
-    <article>
-        <img src="images/myTrips/${myTripsData[i].photo}.png" alt="${myTripsData[i].photo}" width="285" height="200" id="photo">
-        <div class="flex">
-            <div class="left">
-                <h3 class="name">${myTripsData[i].name}</h3>
-                <div>
-                    <img src="images/icons/rating.png" alt="rating" width="82" height="20">
-                    <p>${myTripsData[i].rating} (${myTripsData[i].reviewsCount} Reviews)</p>
-                </div>
-                <p><span>Non refundable</span></p>
-                <p>Check in: ${myTripsData[i].checkIn}</p>
-                <p>Check out: ${myTripsData[i].checkOut}</p>
-                <p>${myTripsData[i].night} night stay</p>
-            </div>
-            <div class="right">
-                <p>1 room 2 days</p>
-                <div>
-                    ${myTripsData[i].oldPrice ? `<p>${myTripsData[i].oldPrice}</p>` : ""}
-                    <h4>$${myTripsData[i].price}</h4>
-                </div>
-                <p>Includes taxes and fees</p>
-                <a href="#"><p class="button">View trip details</p></a>
-            </div>
-        </div>
-    </article>`
+async function cargarSupaBase(){
+    let myTripsRes = await client.from("myTrips").select("*")
+    let myTrips = myTripsRes.data || []
+    generarMyTrips(myTrips)
+
+    let recomendadosRes = await client.from("destinacionsDestacades").select("*")
+    let recomendados = recomendadosRes.data || []
+    generarRecomendados(recomendados)
 }
 
 
-const sectionCards = document.querySelector(".section-cards")
-for(let i = 0; i < destinacionsDestacades.length ; i++){
-    sectionCards.innerHTML += `
-    <article>
-        <div class="history">
-            <img src="${destinacionsDestacades[i].imatgeUrl}" alt="${destinacionsDestacades[i].nom}">
-        </div>
-        <h4 class="name">${destinacionsDestacades[i].nom}</h4>
-        <p class="properties">${destinacionsDestacades[i].propietatsCount} properties</p>
-    </article>`; 
+function generarMyTrips(myTrips){
+    const myTripsContainer = document.querySelector("#tripsDiv")
+    for(let i = 0; i < myTrips.length; i++){
+        myTripsContainer.innerHTML += `
+        <article>
+            <img src="images/myTrips/${myTrips[i].id}.png" alt="${myTrips[i].id}" width="285" height="200" id="photo">
+            <div class="flex">
+                <div class="left">
+                    <h3 class="name">${myTrips[i].name}</h3>
+                    <div>
+                        <img src="images/icons/rating.png" alt="rating" width="82" height="20">
+                        <p>${myTrips[i].rating} (${myTrips[i].reviewsCount} Reviews)</p>
+                    </div>
+                    <p><span>Non refundable</span></p>
+                    <p>Check in: ${myTrips[i].checkIn}</p>
+                    <p>Check out: ${myTrips[i].checkOut}</p>
+                    <p>${myTrips[i].night} night stay</p>
+                </div>
+                <div class="right">
+                    <p>1 room 2 days</p>
+                    <div>
+                        ${myTrips[i].oldPrice ? `<p>${myTrips[i].oldPrice}</p>` : ""}
+                        <h4>$${myTrips[i].price}</h4>
+                    </div>
+                    <p>Includes taxes and fees</p>
+                    <a href="#"><p class="button">View trip details</p></a>
+                </div>
+            </div>
+        </article>
+        `
+    }
 }
+
+
+function generarRecomendados(recomendados){
+    const recomendadosContainer = document.querySelector(".section-cards")
+    console.log(recomendadosContainer)
+    for(let i = 0; i < recomendados.length; i++){
+        recomendadosContainer.innerHTML += `
+        <article>
+            <div class="history">
+                <img src="../images/locations/${recomendados[i].id}.png" alt="${recomendados[i].nom}">
+            </div>
+            <h4 class="name">${recomendados[i].nom}</h4>
+            <p class="properties">${recomendados[i].propietatsCount} properties</p>
+        </article>`
+    }
+}
+
+cargarSupaBase()
