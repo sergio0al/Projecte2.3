7f2ef3a2a3ff88ae38d2322fe55c3795f9d8406b
Sergio Alonso <alonsorojalssergio@fpllefia.com>
2026-04-20 22:23:47 +0200

    fin checkoutSupaBase

--- js/checkout.js
+++ js/checkout.js
@@ -1,36 +1,43 @@
-import { checkoutData } from "./data.js"
+let supabaseUrl = "https://ndxpcylicovkaaoxydhk.supabase.co";  
+let supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5keHBjeWxpY292a2Fhb3h5ZGhrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzYzNDMwMTAsImV4cCI6MjA5MTkxOTAxMH0.eCodC6u0WsPGsJk-CxBTRdyXA9E0xgwxLrWiNgjYWpE";
+let client = supabase.createClient(supabaseUrl, supabaseAnonKey);
 
-let price = 0
-let html = ""
-const right = document.querySelector(".right")
-
-for(let i = 0; i < checkoutData.length; i++){
-    
-    html += `
-    <div class="hotel">
-        <div class="borderPhoto">
-            <img src="images/${checkoutData[i].photo}.png" alt="Lakeside Motel Warefront" class="hotelPhoto">
-        </div>
-        <h3>${checkoutData[i].name}</h3>
-        <div>
-            <img src="images/icons/rating.png" alt="stars" width="82" height="20" class="start">
-            <p>${checkoutData[i].rating} (${checkoutData[i].reviewsCount} Reviews)</p>
-        </div>
-        <p><span>Non refundable</span></p>
-        <p>Check in: ${checkoutData[i].checkIn}</p>
-        <p>Check out: ${checkoutData[i].checkOut}</p>
-        <p>${checkoutData[i].night} night stay</p>
-    </div>`
-
-    price += checkoutData[i].price
+async function cargarSupaBase(){
+    let checkoutRes = await client.from("checkout").select("*")
+    let checkout = checkoutRes.data || []
+    generarCheckout(checkout)
 }
 
 
-right.innerHTML = html
+function generarCheckout(checkout){
+    let price = 0
+    let html = ""
+    const right = document.querySelector(".right")
+    
+    for(let i = 0; i < checkout.length; i++){
+    
+    html += `
+        <div class="hotel">
+            <div class="borderPhoto">
+                <img src="images/${checkout[i].id}.png" alt="Lakeside Motel Warefront" class="hotelPhoto">
+            </div>
+            <h3>${checkout[i].name}</h3>
+            <div>
+                <img src="images/icons/rating.png" alt="stars" width="82" height="20" class="start">
+                <p>${checkout[i].rating} (${checkout[i].reviewsCount} Reviews)</p>
+            </div>
+            <p><span>Non refundable</span></p>
+            <p>Check in: ${checkout[i].checkIn}</p>
+            <p>Check out: ${checkout[i].checkOut}</p>
+            <p>${checkout[i].night} night stay</p>
+        </div>`
+        price += checkout[i].price
+    }
 
+    right.innerHTML = html
 
-right.innerHTML += `
-<h2 class="titledetails">Price Details</h2>
+    right.innerHTML += `
+    <h2 class="titledetails">Price Details</h2>
     <div class="pricedetails">
         <div>
             <p>1 room X 2 nights</p>
@@ -54,3 +61,6 @@ right.innerHTML += `
             </div>
         </section>
     </div>`
+}
+
+cargarSupaBase()
