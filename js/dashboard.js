6fc9f19946e1ccd70f715c0d8ef0b39413642681
Sergio Alonso <108427165+sergio0ar@users.noreply.github.com>
2026-04-28 22:47:45 +0200

    mejoraFormulario

--- js/dashboard.js
+++ js/dashboard.js
@@ -11,6 +11,7 @@ let rooms = document.querySelector("#rooms")
 let days = document.querySelector("#days")
 
 let addButton = document.querySelector("#addButton")
+let errorMessage = document.querySelector("#error")
 
 addButton.addEventListener('click', async function(e){ 
 
@@ -30,7 +31,17 @@ addButton.addEventListener('click', async function(e){
         photo: photo.value,
     }
 
-    await client.from('resultados').insert([newData])
+    let {data, error} = await client.from('resultados').insert([newData])
 
+    if(error){
+        errorMessage.textContent = "Algo ha salido mal! No se ha podiod añair tu anunció."
+    }else{
+        errorMessage.textContent = ""
+        name.value = ""
+        description.value = ""
+        price.value = ""
+        photo.value = ""
+        
+    }
 
 })
